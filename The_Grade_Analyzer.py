a8540ffd01fd77d7622c7efc13974c9c8955be1f
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-04-17 09:52:32 -0500

    Add files via upload

---
+++ The_Grade_Analyzer.py
@@ -0,0 +1,8 @@
+my_nums = [88, 90, 67, 75, 83, 70, 61, 94]
+total = sum(my_nums)
+print('average of my_nums:', total / 8)
+
+highest_grade = my_nums[7:]
+print('highest grade:', highest_grade)
+lowest_grade = my_nums[6:7]
+print('lowest grade:', lowest_grade)
